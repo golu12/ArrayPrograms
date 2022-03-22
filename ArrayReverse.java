9e8361ae61315f8d46badafbc08d15685a0ae263
golu12 <shivam.kesherwani@gmail.com>
2022-03-22 19:35:09 +0100

    Update ArrayReverse.java

--- ArrayReverse.java
+++ ArrayReverse.java
@@ -1,1 +1,22 @@
 
+import java.util.Arrays;
+
+public class functionTest {
+    public static void main(String args[]) {
+        int[] data = {3, 5, 4, 8, 11, 1, 4, 6};
+        int [] reverse = new int[data.length];
+        int firstElement = 0;
+        int lastElement = data.length-1;
+
+        while(firstElement < lastElement){
+            int temp = data[firstElement];
+            data[firstElement] = data[lastElement];
+            data[lastElement] = temp;
+            firstElement++;
+            lastElement--;
+
+
+        }
+        System.out.println(Arrays.toString(data));
+    }
+}
