860d11cf8ae70a8740eff14b3927ad81c5ce072f
golu12 <shivam.kesherwani@gmail.com>
2022-03-22 20:37:53 +0100

    Create Average.java

---
+++ Average.java
@@ -0,0 +1,14 @@
+package com.international.bank.demo.service;
+
+
+public class functionTest {
+    public static void main(String args[]) {
+        int[] data = {3, 5, 4, 8, 11, 1, 4, 6};
+        int sum = 0;
+        for (int i = 0; i < data.length ; i++) {
+            sum += data[i];
+
+        }
+    System.out.println(sum/2);
+    }
+}
